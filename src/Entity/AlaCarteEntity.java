1dc23c04a8b258972e9d8ada07ea3a6fe3178aa4
Goh Xue Zhe <gohxuezhe@gmail.com>
2021-11-11 21:28:31 +0800

    Java document AlaCarteEntity

--- src/Entity/AlaCarteEntity.java
+++ src/Entity/AlaCarteEntity.java
@@ -2,63 +2,133 @@ package Entity;
 
 import java.io.Serializable;
 
-// this is a single alacarte object
+/**
+ * 
+ * Is a single AlaCarte object
+ * 
+ * @author Wong Wei Bin
+ * @author Ivan Teo
+ * @author Grace Wong
+ * @author Goh Xue Zhe
+ * @author Matthew Chua
+ * @version 0.1.0
+ * @since 2021-11-11
+ */
 public class AlaCarteEntity implements Serializable {
-    public enum Type{
-        APPETISER(1),
-        MAINCOURSE(2),
-        DRINK(3),
-        DESSERT(4);
+    /**
+     * The different types of AlaCarte object
+     */
+    public enum Type {
+        APPETISER(1), MAINCOURSE(2), DRINK(3), DESSERT(4);
 
         int num;
+
         Type(int n) {
             num = n;
         }
     }
-    //instantiate attributes
+
+    /**
+     * The name of this AlaCarte item
+     */
     private String name;
+    /**
+     * The description of this AlaCarte item
+     */
     private String desc;
+    /**
+     * The price of this AlaCarte item
+     */
     private double price;
+    /**
+     * The type of this AlaCarte item
+     */
     private Type type;
 
-    // constructor
-    public AlaCarteEntity(String name, String desc, double price, Type type){
+    /**
+     * Creates a new AlaCarte item with name
+     * 
+     * @param name  This item's name
+     * @param desc  This item's description
+     * @param price This item's price
+     * @param type  This item's type
+     */
+    public AlaCarteEntity(String name, String desc, double price, Type type) {
         this.name = name;
         this.desc = desc;
         this.price = price;
         this.type = type;
     }
 
-    public String getName(){
+    /**
+     * Gets the name of this AlaCarte item
+     * 
+     * @return this AlaCarte item's name
+     */
+    public String getName() {
         return this.name;
     }
 
-    public String getDesc(){
+    /**
+     * Gets the description of this AlaCarte item
+     * 
+     * @return this AlaCarte item's description
+     */
+    public String getDesc() {
         return this.desc;
     }
 
-    public double getPrice(){
+    /**
+     * Gets the price of this AlaCarte item
+     * 
+     * @return this AlaCarte item's price
+     */
+    public double getPrice() {
         return this.price;
     }
 
-    public Type getType(){
+    /**
+     * Gets the type of this AlaCarte item
+     * 
+     * @return this AlaCarte item's type
+     */
+    public Type getType() {
         return this.type;
     }
 
-    public void setType(Type inputType){
-        this.type = inputType;
-    }
-
-    public void setName(String inputName){
+    /**
+     * Changes the name of this AlaCarte item
+     * 
+     * @param inputName This AlaCarte item's new name
+     */
+    public void setName(String inputName) {
         this.name = inputName;
     }
 
-    public void setDesc(String inputDesc){
+    /**
+     * Changes the description of this AlaCarte item
+     * 
+     * @param inputDesc This AlaCarte item's new description
+     */
+    public void setDesc(String inputDesc) {
         this.desc = inputDesc;
     }
 
-    public void setPrice(double inputPrice){
+    /**
+     * Changes the price of this AlaCarte item
+     * 
+     * @param inputPrice This AlaCarte item's new price
+     */
+    public void setPrice(double inputPrice) {
         this.price = inputPrice;
     }
-}
 
+    /**
+     * Changes the type of this AlaCarte item
+     * 
+     * @param inputType This AlaCarte item's new type
+     */
+    public void setType(Type inputType) {
+        this.type = inputType;
+    }
+}
