faf115007aba5ee60cc2325163cbdd5f35f6030f
Goh Xue Zhe <gohxuezhe@gmail.com>
2021-11-11 21:44:29 +0800

    Java document InvoiceEntity

--- src/Entity/InvoiceEntity.java
+++ src/Entity/InvoiceEntity.java
@@ -1,80 +1,92 @@
 package Entity;
 
 import java.io.Serializable;
-// import java.sql.Date;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
-import java.util.HashMap;
-// import java.sql.Date;
-import java.util.Dictionary;
 
 import Boundary.Boundary;
 
-
+/**
+ * 
+ * Is a single Invoice object
+ * 
+ * @author Wong Wei Bin
+ * @author Ivan Teo
+ * @author Grace Wong
+ * @author Goh Xue Zhe
+ * @author Matthew Chua
+ * @version 0.1.0
+ * @since 2021-11-11
+ */
 public class InvoiceEntity implements Serializable{
-
-    private String StaffEntityName; // need on receipt?
-    // private LocalDateTime timestamp; // or the order time?
-    private Date timestamp; // or the order time?
-    private int tableNumber; // get from Table class?
-    // private ArrayList<AlaCarteEntity> menuItems; // get from Order class?
-    // private ArrayList<PackageEntity> packageItems;
-    
+    /**
+     * The time that the invoice is created
+     */
+    private Date timestamp;
+    /**
+     * The table assigned to the order
+     */
+    private int tableNumber;
+    /**
+     * The menu items ordered
+     */
     private Map<AlaCarteEntity, Integer> menuItems;
+    /**
+     * The package items ordered
+     */
     private Map<PackageEntity, Integer> packageItems;
-
-    private double paymentDue; // do in order or invoice
-    private Boolean membership; // checking here?
-    // private static String separators = "============";
+    /**
+     * The membership status of the customer
+     */
+    private Boolean membership;
     
 
-    // constructor
+    /**
+     * Creates a new Invoice with name
+     * @param staffName This Invoice's staff assigned
+     * @param tableNumber This Invoice's table number
+     * @param menuItems This Invoice's menu items ordered
+     * @param packageItems This Invoice's package items ordered
+     * @param membership This Invoice's customer's membership status
+     */
     public InvoiceEntity(String staffName, int tableNumber, Map<AlaCarteEntity, Integer> menuItems, Map<PackageEntity, Integer> packageItems, Boolean membership) {
         this.StaffEntityName = staffName;
-        // this.timestamp = LocalDateTime.now();
-        // SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         long millis=System.currentTimeMillis(); 
         this.timestamp = new Date(millis);
-        this.membership = membership;
+        this.tableNumber = tableNumber;
         this.menuItems = menuItems;
         this.packageItems = packageItems;
-        this.tableNumber = tableNumber;
+        this.membership = membership;
     }
 
+    /**
+     * Gets the time stamp of this Invoice
+     * @return this Invoice's time stamp
+     */
     public Date getTimeStamp(){
         return this.timestamp;
     }
 
+    /**
+     * Gets the menu items of this Invoice
+     * @return this Invoice's menu items
+     */
     public Map<AlaCarteEntity, Integer> getAlaCarteItems(){
         return this.menuItems;
     }
 
+    /**
+     * Gets the package items of this Invoice
+     * @return this Invoice's package items
+     */
     public Map<PackageEntity, Integer> getPackageItems(){
         return this.packageItems;
     }
 
-    // public void printInvoice(){
-    //     System.out.println("INVOICE FOR TABLE NO: " + this.tableNumber);
-    //     System.out.println("ALCARTE ITEMS:");
-    //     for (int i=0; i<this.menuItems.size(); i++){
-    //         System.out.println(this.menuItems.get(i).getName()+"-------------"+this.menuItems.get(i).getPrice());
-    //     }
-
-    //     System.out.println("PACKAGE ITEMS:");
-    //     for (int i=0; i<this.packageItems.size(); i++){
-    //         System.out.println(this.packageItems.get(i).getName()+"-------------"+this.packageItems.get(i).getPrice());
-    //     }
-
-    //     float total = calculateTotal();
-
-    //     System.out.println("TOTAL W/O TAX: " + total);
-    //     System.out.println("TOTAL  TAX: " + total*1.17);
-    //     if (this.membership){
-    //         System.out.println("TOTAL AFTER MEMBERSHIP DISCOUNT: " + total*1.17*0.9);
-    //     }
-    // }
+    /**
+     * Collates receipt details into a single string to display
+     * @return string to display when concluding order
+     */
     String invoiceString = "INVOICE FOR TABLE NO: ";
     float total;
     public String toString(){
@@ -87,28 +99,13 @@ public class InvoiceEntity implements Serializable{
             invoiceString += qtyString + " x ";
             String orderName = alcItem.getName();
             String updatedOrderName = ((orderName.length() > 25) ? (orderName.substring(0, 24) + "...") : orderName);
-            // String price = String.valueOf(order.getMenuItems().get(i).getPrice());
             String price = String.format("%.2f", alcItem.getPrice());
             String alignedPrice = String.format("%" + String.valueOf(40 -  updatedOrderName.length())+ "s", price);
             invoiceString += updatedOrderName + alignedPrice + "\n";
         });
 
-        // for (int i=0; i<this.menuItems.size(); i++){
-        //     invoiceString += this.menuItems.get(i).getName()+"\t\t\t"+this.menuItems.get(i).getPrice() + "\n";
-        // }
         invoiceString += "\nPACKAGE ITEMS:\n\n";
-        // for (int i=0; i<this.packageItems.size(); i++){
-        //     invoiceString += this.packageItems.get(i).getName()+ "\t\t\t" + String.valueOf(this.packageItems.get(i).getPrice()) + "\n";
-        // }
-
-        // float total = calculateTotal();
-        // invoiceString += "\nTOTAL W/O TAX: " + String.valueOf(total) + "\n";
-        // invoiceString += "TOTAL  TAX: " + String.valueOf(total*1.17) + "\n";
-        // if (this.membership){
-        //     invoiceString += "TOTAL AFTER MEMBERSHIP DISCOUNT: " + String.valueOf(total*1.17*0.9) + "\n";
-        // }
-
-        // copied
+        
         total = 0;
         this.packageItems.forEach((k, v) -> total += (k.getPrice() * v ) );
         this.menuItems.forEach((k, v) -> total += (k.getPrice() * v ));
@@ -132,25 +129,17 @@ public class InvoiceEntity implements Serializable{
             invoiceString += membershipString + alignedPrice + "\n";
         }
         String toReturn = invoiceString;
-        // orderString = "ALA CARTE ITEMS:\n";
-        
-        String invoiceString = "INVOICE FOR TABLE NO: ";
         total = 0;
 
         return toReturn;
     }
 
+    /**
+     * Calculate the price per Invoice
+     * @return this Invoice's total price
+     */
     double totalRevenue = 0;
     public double calculateTotal(){
-
-        
-
-        // for (int i=0; i<this.packageItems.size(); i++){
-        //     total += packageItems.get(i).getPrice();
-        // }
-        // for (int i=0; i<this.menuItems.size(); i++){
-        //     total += menuItems.get(i).getPrice();
-        // }
         this.packageItems.forEach((item, qty) -> totalRevenue += (item.getPrice() * qty));
         this.menuItems.forEach((item, qty) -> totalRevenue += (item.getPrice() * qty));
         
@@ -162,9 +151,4 @@ public class InvoiceEntity implements Serializable{
         totalRevenue = 0;
         return toReturn;
     }
-    // getters;
-
-
-    // setters
-
 }
